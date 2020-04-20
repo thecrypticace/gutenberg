0ee0f8d81b812201d676fb4ba085c03ccdb9a21f
Noah Allen <noahtallen@gmail.com>
2020-04-20 12:01:55 -0700

    Add command to display Docker container logs

--- packages/env/lib/commands/index.js
+++ packages/env/lib/commands/index.js
@@ -5,10 +5,12 @@ const start = require( './start' );
 const stop = require( './stop' );
 const clean = require( './clean' );
 const run = require( './run' );
+const logs = require( './logs' );
 
 module.exports = {
 	start,
 	stop,
 	clean,
 	run,
+	logs,
 };
